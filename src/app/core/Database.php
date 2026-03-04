fb78189abcff1c15d70693e548bc79e438e3ebf9
ježíš <82285590+eingorz@users.noreply.github.com>
2026-03-04 18:49:01 +0100

    Move timezone config to Database core class

--- src/app/core/Database.php
+++ src/app/core/Database.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set('Europe/Prague');
 class Database {
     private string $dsn;
     private string $user;
--- src/index.php
+++ src/index.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-date_default_timezone_set('Europe/Prague');
 $config = require __DIR__ .'/config/database.php';
 require_once __DIR__ .'/app/core/Database.php';
 require_once __DIR__ .'/app/core/Router.php';
